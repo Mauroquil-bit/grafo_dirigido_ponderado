bd7c8548c3e9471db821f80fd6c4ccbc8fbe399f
Mauricio Mercado <75552002+Mauroquil-bit@users.noreply.github.com>
2024-10-25 23:04:15 -0300

    Update test_grafo_dirig_pond.py

--- test_grafo_dirig_pond.py
+++ test_grafo_dirig_pond.py
@@ -1,1 +1,21 @@
+import unittest
+import networkx as nx
+from grafo_dirig_pond import G, edges
 
+class TestGrafoDirigidoPonderado(unittest.TestCase):
+
+    def setUp(self):
+        self.grafo = G
+
+    def test_numero_de_nodos(self):
+        self.assertEqual(len(self.grafo.nodes), 5, "El número de nodos debería ser 5")
+
+    def test_numero_de_aristas(self):
+        self.assertEqual(len(self.grafo.edges), 7, "El número de aristas debería ser 7")
+
+    def test_pesos_aristas(self):
+        for (u, v, w) in edges:
+            self.assertEqual(self.grafo[u][v]['weight'], w, f"El peso de la arista ({u}, {v}) debería ser {w}")
+
+if __name__ == '__main__':
+    unittest.main()
